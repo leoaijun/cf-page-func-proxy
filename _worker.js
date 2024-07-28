3572514efe1c111c3b7bb300d99098bcad76948e
leoaijun <115625144+leoaijun@users.noreply.github.com>
2024-07-28 19:49:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="fdcf.seein.pp.ua";
+        url.hostname="ctxj166.eicp.net";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
